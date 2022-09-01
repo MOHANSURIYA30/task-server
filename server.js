feb950e081628a7b9cc28d236c3ec0f0dee32b5a
MOHANKUMAR <mohansuriya301198@gmail.com>
2022-09-01 07:59:29 +0530

    new update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000
-
+const cors = require('cors')
 const adminRoutes = require('./routes/admin');
 const employeeRoutes = require('./routes/employee');
 const taskRoutes = require('./routes/task');
@@ -34,21 +34,21 @@ app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({extended: true} ));
 
 // ALTERNATE USER CORS
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+//     );
   
-    next();
+//     next();
     
-  });
-
+//   });
+  app.use(cors())
   app.use("/api/admin",adminRoutes);
   app.use("/api/employee",employeeRoutes);
   app.use("/api/task",taskRoutes);
